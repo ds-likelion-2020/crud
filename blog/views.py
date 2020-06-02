f20f56e5414f4efde9330f60fffd35480d3a6e2f
eunjungleo <asaprocky123@likelion.org>
2020-06-02 17:12:13 +0900

    change timezone to asia/seoul

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from .models import Post
 from .forms import PostForm # forms.py에서 PostForm 가져오기
 
 def detail(request, pk): # request와 pk도 인자로 받음
-    post = get_object_or_404(Post, pk=pk) # 해당 객체가 있으면 가져오고 없으면 404에러, pk값은 blog.id
+    post = get_object_or_404(Post, pk=pk) # 해당 객체가 있으면 가져오고 없으면 404에러, pk값은 pk
     return render(request, 'detail.html', {'post':post})
 
 
